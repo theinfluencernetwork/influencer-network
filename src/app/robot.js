bcac08c166601308519f694bb6bcd90966f22576
kaneki-desu <sibajit.mazumdar@gmail.com>
2025-08-26 12:08:26 +0530

    robot.js initial commit

---
+++ src/app/robot.js
@@ -0,0 +1,14 @@
+import sitemap from "./sitemap";
+
+export default function robots(){
+    return {
+        rules:[
+            {
+                userAgent: '*', 
+                allow: '/' ,
+                disallow: '/admin'
+            }
+        ],
+        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    }
+}
